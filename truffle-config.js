3fcfc73858f89fd85e06e30874a5e088b3748d0a
Shengda <dingshengda@gmail.com>
2020-06-27 00:17:43 -0700

    Fix mainnet config

--- truffle-config.js
+++ truffle-config.js
@@ -53,7 +53,7 @@ module.exports = {
 
     // Another network with more advanced options...
     main: {
-      provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://kovan.infura.io/v3/" + process.env.INFURA_API_KEY, 0, 10),
+      provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://mainnet.infura.io/v3/" + process.env.INFURA_API_KEY, 0, 10),
       network_id: 1,          // Main's id
       gas: 6700000,           // Gas sent with each transaction (default: ~6700000)
       gasPrice: 35000000000,  // 35 gwei (in wei) (default: 100 gwei)
